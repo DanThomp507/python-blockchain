28764bd876a5adf0d6d95a7eff6bcebfe96b46f7
Daniel Thompson <DanThomp507@gmail.com>
2021-05-03 19:10:51 +0100

    feat: set up skeleton of blockchain class

---
+++ app.py
@@ -0,0 +1,24 @@
+class Blockchain(object):
+
+
+def __init__(self):
+    self.chain = []
+    self.current_transactions = []
+
+
+def addBlock(self):
+    pass
+
+
+def addTransaction(self):
+    pass
+
+
+@staticmethod
+def hash(block):
+    pass
+
+
+@property
+def lastBlock(self):
+    pass
